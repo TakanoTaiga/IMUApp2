dcd98c26ef9619c77fe8b2288ec1ae55613cb1f7
TaigaTakano <ttttghghnb554z@outlook.jp>
2022-06-29 09:20:34 +0900

    Initial Commit

---
+++ IMUApp2/IMUApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  IMUApp2App.swift
+//  IMUApp2
+//
+//  Created by 高野大河 on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct IMUApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
